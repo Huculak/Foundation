a9615b6630b217a4f17154d3529331fe21326f28
Huculak <49389190+Huculak@users.noreply.gihub.com>
2019-04-18 23:24:21 -0600

    Part 1 Complete

--- Assignment1.R
+++ Assignment1.R
@@ -1,14 +1,41 @@
 # Created a File
 
-#Create a numeric variable my.age that is equal to your age.
+install.packages("stringr", repos='http://cran.us.r-project.org')
+library(stringr)
 
-my.age <- 22
+# Part 1. Introductions
+
+my.age <- 29
 my.age
 
+my.name <- 'Matt'
+my.name
 
+#Define a function called MakeIntroduction...
 
+MakeIntroduction <- function(name, age) {
+  paste("Hello, my name is", name, ", and I'm ", age, "years old!")
+  
+}
 
+#Create a variable my.intro...
 
+my.intro <- make_birthday_intro(my.name, my.age)
+my.intro
 
-library(tidyverse)
-mpg
+#Create a variable casual.intro...
+
+casual.intro <- sub("Hello, my name is", "Hey, I'm", my.intro)
+casual.intro
+
+#Create a variable capital.intro...
+
+capital.intro <- str_to_upper(casual.intro)
+capital.intro
+
+#Create a variable intro.e.count...
+
+intro.e.count <- str_count(my.intro, pattern = 'e')
+intro.e.count
+  
+
