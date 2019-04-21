cfe380edbea51c849697f876c1de6dc6b1ab2090
Huculak <49389190+Huculak@users.noreply.gihub.com>
2019-04-20 18:53:07 -0600

    Part 3

--- Assignment1.R
+++ Assignment1.R
@@ -3,6 +3,7 @@
 install.packages("stringr", repos='http://cran.us.r-project.org')
 library(stringr)
 
+library(tidyverse)
 # Part 1. Introductions
 
 my.age <- 29
@@ -38,4 +39,44 @@ capital.intro
 intro.e.count <- str_count(my.intro, pattern = 'e')
 intro.e.count
   
+
+#Part 2. Books
+#Create a variable name book...
+
+book <- c("Siddhartha", "The Trial", "Darkness at Noon", "The Joke", "Kidnapped", "Starship Troopers")
+book
+
+#Create a variable top.three.books...
+
+top.three.books <- book[c(1:3)]
+top.three.books
+
+#Create a variable book.reviews...
+
+book.reviews <- paste(book, "is a great read!")
+book.reviews
+
+#Create a variable books.without.four...
+
+books.without.four <- book[c(1:3,5:6)]
+books.without.four
+
+#Create a variable long.titles...
+
+long.titles <- Length[book > 10 chars]
+
+#Part 3 Squares
+
+#Create a variable numbers...
+
+numbers <- (1:201)
+numbers
+
+#Create a variable square.numbers...
+
+squared.numbers <- numbers^2
+squared.numbers
+
+
+
 
